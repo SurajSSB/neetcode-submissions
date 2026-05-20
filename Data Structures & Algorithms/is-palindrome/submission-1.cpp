cb79683db50aafc65a3dd4e96f27405dbae27d87
SurajSSB <surajbade24@gmail.com>
2026-05-20 22:53:45 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char> str;
+        for (char ch : s)
+        {
+            int ascii = int(ch);
+            if((ascii  >= 48 && ascii <= 57) ||
+            (ascii  >= 65 && ascii <= 90) ||
+            (ascii  >= 97 && ascii <= 122))
+            {
+                     str.push_back(tolower(ch));
+            }   
+        }
+        vector<char> revStr (str);
+        reverse(revStr.begin(), revStr.end()); 
+        if(revStr == str)
+        return true;
+
+        return false;
+    }
+};
