213a257a4e9e955a643cbfa9bfda78229969a5a9
SurajSSB <surajbade24@gmail.com>
2026-05-21 19:09:30 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+       set<vector<int>> res;
+       vector<vector<int>> res1;
+        sort(nums.begin(), nums.end());
+
+        //O(n3))
+        /*for (int i = 0; i < nums.size(); i++) {
+             cout << nums[i] << ", ";
+            for (int j = i + 1; j < nums.size(); j++) {
+                for (int k = j + 1; k < nums.size(); k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        res.insert({nums[i], nums[j], nums[k]});
+                    }
+                }
+            }
+        }*/
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int  l = i +1, r = nums.size() - 1;
+            int sum = -nums[i];
+
+            if (nums[i] > 0) break; //only consider negative numbers
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip same numbers
+
+            while( l < r /*&& r > i && l < nums.size()*/)
+            {
+                if((nums[l] + nums[r] ) == sum)
+                {
+                    //Insert triplet                    
+                    res.insert({nums[i], nums[l], nums[r]});
+                    res1.push_back({nums[i], nums[l], nums[r]});
+                     l++;
+                     r--;
+                     cout << "l= " << l <<", r= " << r <<"\n";
+                     while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                        cout << "l++\n";
+                    }
+                }
+                else if((nums[l]+ nums[r]) > sum)
+                    r--;
+                else
+                    l++;
+            }
+        }
+
+        return res1;
+        return vector<vector<int>>(res.begin(), res.end());
+        
+    }
+};
