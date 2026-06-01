b4bacf35f46d0d80a039efd133424c733d1b9c2e
SurajSSB <surajbade24@gmail.com>
2026-06-01 18:52:32 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        std::stack<char> braces;
+        for(char ch : s)
+        {
+            if(ch == '(' || ch == '[' || ch == '{')
+            {  
+                braces.push(ch);
+                //cout << ch << ", ";
+            }
+            else if(ch == ')' || ch == ']' || ch == '}')
+            {
+                char lastChar = '\0';
+                if(!braces.empty())
+                {
+                    lastChar= braces.top();
+                    braces.pop();
+                }
+
+                if(ch == ')' && lastChar == '(')
+                    continue;
+
+                 if(ch == ']' && lastChar == '[')
+                    continue;
+
+                 if(ch == '}' && lastChar == '{')
+                    continue;
+
+                 return false;
+            }
+        }
+
+        if(braces.empty())
+            return true;
+        else
+            return false;        
+    }
+};
